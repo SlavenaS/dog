5c6434aa9ef67d13ea1853b35c844ef1af6a267c
Slavena Slavova <106436147+SlavenaS@users.noreply.github.com>
2023-10-05 13:06:31 +0300

    Add files via upload

---
+++ konstruktor/konstruktor/Dog.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace konstruktor
+{
+    public class Dog
+    {
+		private int age;
+
+		public int Age
+		{
+			get { return age; }
+			set { age = value; }
+		}
+		private string name;
+
+		public string Name
+		{
+			get { return name; }
+			set { name = value; }
+		}
+		private double kg;
+
+		public double Kg
+		{
+			get { return kg; }
+			set { kg = value; }
+		}
+		public Dog()
+		{
+			this.age = 3;
+			this.name = "Maya";
+			this.kg = 10;
+		}
+		//:this nasledqva ot gorniq ctor
+		public Dog(int age):this()
+		{
+			this.age=age;
+		}
+		public Dog(int age, string name) : this(age)
+		{
+			this.name = name;
+		}
+		public Dog(int age,string name,double kg):this(age,name)
+		{
+			this.kg=kg;
+		}
+		public void Print()
+		{
+            Console.WriteLine($"{age} {name} {kg}");
+        }
+
+	}
+}
---
+++ konstruktor/konstruktor/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace konstruktor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dog dog1 = new Dog();
+            dog1.Print();
+            Console.Write("vuvedi godini ");
+            Dog dog2 = new Dog(int.Parse(Console.ReadLine()));
+            dog2.Print();
+            Console.Write("vuvedi ime ");
+            Dog dog3 = new Dog(int.Parse(Console.ReadLine()),Console.ReadLine());
+            dog3.Print();
+            Console.Write("vuvedi kg ");
+            Dog dog4 = new Dog(int.Parse(Console.ReadLine()), Console.ReadLine(),double.Parse(Console.ReadLine()));
+            dog4.Print();
+            List<Dog>list = new List<Dog> { dog1, dog2, dog3, dog4 };
+
+        }
+    }
+}
